3792f5b8b1e3dae66bd1339a2df5ecc64c4658a0
Pavel Konkin <idmfani@gmail.com>
2023-06-20 17:20:07 +0300

    ldskhcbdlvb

--- app/libs/eslint_api.rb
+++ app/libs/eslint_api.rb
@@ -11,7 +11,7 @@ class EslintApi
     Open3.capture2("git clone #{repo['clone_url']} #{Rails.root.join(repo[:name])}")
     check.start!
     # conf_path = Rails.root.join('.eslintrc.yml')
-    check_result, _status = Open3.popen3('ls -al') { |_stdin, stdout, _stderr, wait_thr| [stdout.read, wait_thr.value] }
+    check_result, _status = Open3.popen3('ls node_modules/eslint/bin') { |_stdin, stdout, _stderr, wait_thr| [stdout.read, wait_thr.value] }
     processed_check_result = []
     processed_check_result << check_result
     check.update(check_result: processed_check_result)
