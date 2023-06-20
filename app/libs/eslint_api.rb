1b2f77717cc0f1008ba44340b4f1610efe6ea9b1
Pavel Konkin <idmfani@gmail.com>
2023-06-21 00:03:37 +0300

    Attempt to eslint check in deploy

--- app/libs/eslint_api.rb
+++ app/libs/eslint_api.rb
@@ -8,20 +8,20 @@ class EslintApi
     if File.directory?(folder_path)
       FileUtils.rm_rf(repo[:name])
     end
-    Open3.capture2("git clone #{repo['clone_url']} #{Rails.root.join(repo[:name])}")
+    Open3.capture2("git clone #{repo['clone_url']}")
     check.start!
     # conf_path = Rails.root.join('.eslintrc.yml')
-    processed_check_result = []
-    check_result, _status = Open3.popen3('ls node_modules/eslint/bin') { |_stdin, stdout, _stderr, wait_thr| [stdout.read, wait_thr.value] }
-    processed_check_result << check_result
-    check_result, _status = Open3.popen3("ls #{folder_path}") { |_stdin, stdout, _stderr, wait_thr| [stdout.read, wait_thr.value] }
-    processed_check_result << check_result
-    check.update(check_result: processed_check_result)
-    check_result, _status = Open3.popen3("node_modules/eslint/bin/eslint.js #{Rails.root.join(repo[:name])} -f json") { |_stdin, stdout, _stderr, wait_thr| [stdout.read, wait_thr.value] }
-    # check_pass = status.exitstatus.zero?
-    check_pass = File.directory?('node_modules/eslint/bin')
+    # processed_check_result = []
+    # check_result, _status = Open3.popen3('ls node_modules/eslint/bin') { |_stdin, stdout, _stderr, wait_thr| [stdout.read, wait_thr.value] }
+    # processed_check_result << check_result
+    # check_result, _status = Open3.popen3("ls #{folder_path}") { |_stdin, stdout, _stderr, wait_thr| [stdout.read, wait_thr.value] }
+    # processed_check_result << check_result
+    # check.update(check_result: processed_check_result)
+    check_result, status = Open3.popen3("node_modules/eslint/bin/eslint.js #{repo[:name]} -f json") { |_stdin, stdout, _stderr, wait_thr| [stdout.read, wait_thr.value] }
+    check_pass = status.exitstatus.zero?
+    # check_pass = File.directory?('node_modules/eslint/bin')
     check.complete!
-    check.update(check_pass:)
+    # check.update(check_pass:)
     processed_check_result = []
 
     error_count = 0
